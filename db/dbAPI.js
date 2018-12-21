11d82de530d7069b24fe16481d8ad9fc32d4db0e
Burke <burkegreen@gmail.com>
2018-12-21 14:32:25 -0800

    broken auth. go back!

--- db/dbAPI.js
+++ db/dbAPI.js
@@ -1,7 +1,36 @@
 const path = require('path');
 const db = require(path.join(__dirname, 'index'));
 
-const getTasksByUser = function getTasksByUser (params, callback) {
+let records = [
+    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
+  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+];
+
+const findById = function(id, cb) {
+  process.nextTick(function() {
+    var idx = id - 1;
+    if (records[idx]) {
+      cb(null, records[idx]);
+    } else {
+      cb(new Error('User ' + id + ' does not exist'));
+    }
+  });
+}
+
+const findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.username === username) {
+        return cb(null, record);
+      }
+    }
+    return cb(null, null);
+  });
+}
+
+
+const getTasksByUser = function (params, callback) {
   db.query('select * from tasks where userID = ?', params, (err, data) => {
     if (err) {
       throw err;
@@ -11,7 +40,7 @@ const getTasksByUser = function getTasksByUser (params, callback) {
   });
 };
 
-const postTaskByUser = function postTaskByUser (params, callback) {
+const postTaskByUser = function (params, callback) {
   db.query('insert into tasks (taskDate, duration, subject, assignment, notes, userID) values (?, ?, ?, ?, ?, ?)', params, (err, data) => {
     if (err) {
       throw err;
@@ -31,4 +60,6 @@ const createUser = function createUser (params, callbak) {
   })
 }
 
-module.exports = { getTasksByUser, postTaskByUser };
+module.exports = { getTasksByUser, postTaskByUser, findByUsername, findById };
+
+
